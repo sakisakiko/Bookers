2cb3f47980c49606aa28e99b857ab57a912d3572
“sakisakiko” <108504337+sakisakiko@users.noreply.github.com@users.noreply.github.com>
2022-07-15 03:43:01 +0000

    [Update]バリデーションの実装途中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,9 +10,13 @@ class BooksController < ApplicationController
   end
 
   def create
-   @book=Book.new(book_params)
-   @book.save
-    redirect_to book_path(@book.id)
+    @book=Book.new(book_params)
+   if @book.save
+     redirect_to book_path(@book.id)
+   else
+     @books=Book.all
+     render:index
+   end
   end
 
 
@@ -25,10 +29,15 @@ class BooksController < ApplicationController
    @book.update(book_params)
    redirect_to book_path(@book.id)
   end
+  
+  def destroy
+   @book=Book.find(params[:id])
+   @book.destroy
+   redirect_to books_path
+  end
 
   private
    def book_params
     params.require(:book).permit(:title, :body)
    end
-
 end
