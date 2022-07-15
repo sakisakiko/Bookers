6aa58623c815bf8bb0c475e42172964843c774f3
“sakisakiko” <108504337+sakisakiko@users.noreply.github.com@users.noreply.github.com>
2022-07-15 09:07:37 +0000

    [Finish]Bookers 確認前

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,6 +12,7 @@ class BooksController < ApplicationController
   def create
     @book=Book.new(book_params)
    if @book.save
+     flash[:notice]="Book was successfully created."
      redirect_to book_path(@book.id)
    else
      @books=Book.all
@@ -26,14 +27,18 @@ class BooksController < ApplicationController
 
   def update
    @book=Book.find(params[:id])
-   @book.update(book_params)
-   redirect_to book_path(@book.id)
+   if @book.update(book_params)
+    redirect_to book_path(@book.id)
+   else
+    render:edit
+   end
   end
   
   def destroy
    @book=Book.find(params[:id])
    @book.destroy
-   redirect_to books_path
+     flash[:notice]="Book was successfully destroyed."
+    redirect_to books_path
   end
 
   private
