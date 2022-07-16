cf10747ae143cfb31e01a60880a09088ea7a3ed3
“sakisakiko” <108504337+sakisakiko@users.noreply.github.com@users.noreply.github.com>
2022-07-16 02:21:52 +0000

    [Finish-Update]Bookers 終了後一部変更

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -28,16 +28,17 @@ class BooksController < ApplicationController
   def update
    @book=Book.find(params[:id])
    if @book.update(book_params)
+    flash[:notice]="Book was successfully updated."
     redirect_to book_path(@book.id)
    else
     render:edit
    end
   end
-  
+
   def destroy
    @book=Book.find(params[:id])
-   @book.destroy
-     flash[:notice]="Book was successfully destroyed."
+    @book.destroy
+    flash[:notice]="Book was successfully destroyed."
     redirect_to books_path
   end
 
